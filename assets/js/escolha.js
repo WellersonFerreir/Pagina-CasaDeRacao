584ef5e291dd79cb4d406316b393b6ebb0dee964
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-02-07 17:46:35 -0300

    Add files via upload

--- assets/js/escolha.js
+++ assets/js/escolha.js
@@ -1,7 +1,15 @@
-// input de pesquisa
-const searchbutton = getElementById("search-button");
+const searchInput = document.querySelector('#searchInput');
+const list = document.querySelector('#list');
 
-documen.addEventListener("click", function(){
-    // código a ser executado quando o botão for clicado
-    alert("oi")
+searchInput.addEventListener('input', function() {
+  const searchValue = this.value.toLowerCase();
+  const listItems = list.querySelectorAll('li');
+
+  listItems.forEach(function(listItem) {
+    if (listItem.textContent.toLowerCase().indexOf(searchValue) !== -1) {
+      listItem.style.display = 'block';
+    } else {
+      listItem.style.display = 'none';
+    }
   });
+});
