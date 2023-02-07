178b0c6b8217541c7fadec8f2b4ca4f41c612acb
Wellerson Ferreira <117539408+WellersonFerreir@users.noreply.github.com>
2023-02-06 23:38:14 -0300

    Add files via upload

---
+++ assets/js/escolha.js
@@ -0,0 +1,7 @@
+// input de pesquisa
+const searchbutton = getElementById("search-button");
+
+documen.addEventListener("click", function(){
+    // código a ser executado quando o botão for clicado
+    alert("oi")
+  });
